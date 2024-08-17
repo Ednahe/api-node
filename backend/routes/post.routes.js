e13fdcd4f4348b2597396b1d7b9e0138a1b5f948
Manu <emmanuel.donahue@hotmail.fr>
2024-08-17 13:44:47 +0200

    suite

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const { setPosts } = require('../controllers/post.controller');
 const router = express.Router();
  
 router.get("/", (req, res) => {
     res.json({message: "data + test"})
 });
 
-router.post("/", (req, res) => {   
-    res.json({message: req.body.message})
-});
+router.post("/", setPosts);
 
 router.put('/:id', (req, res) => {
     res.json({messageId: req.params.id})
@@ -19,10 +18,10 @@ router.delete("/:id", (req, res) => {
 
 router.patch("/like-post/:id", (req, res) => {
     res.json({message: "Post liké : id " + req.params.id})
-})
+});
 
 router.patch("/dislike-post/:id", (req, res) => {
     res.json({message: "Post disliké : id " + req.params.id})
-})
+});
 
 module.exports = router;
