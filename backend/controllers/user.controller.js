84894fa931c913836632ae205a1711e7538673c0
Manu <emmanuel.donahue@hotmail.fr>
2024-08-23 16:02:41 +0200

    ajout logique utilisateur

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,81 @@
+const User = require('../models/user.model');
+
+module.exports.registerUser = async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+
+        const userNotValide = await User.findOne({email});
+        if (userNotValide) {
+            return res.status(400).json({ message: "Cet email est déjà utilisé."})
+        }
+
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+
+        res.status(201).json({ message: 'Utilisateur créé avec succès.', user: newUser});
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la création utilisateur.', error: error.message});
+    }
+};
+
+module.exports.loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({ message: 'Email incorrect.'})
+        }
+
+        const passwordValide = await user.isPasswordValid(password);
+        if(!passwordValide) {
+            return res.status(400).json({ message: 'mort de passe incorrect.'})
+        }
+
+        res.status(200).json({ message: 'Connexion réussie', user})
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la connexion.', error: error.message });
+    }
+};
+
+module.exports.getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user) {
+            return res.status(404).json({ message: 'utilisateur non trouvé'})
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la récupération de l\'utilisateur.', error: error.message });
+    }
+};
+
+module.exports.updateUser = async (req, res) => {
+    try {
+        const updates = req.body;
+        const user = await User.findByIdAndUpdate(req.params.id, updates, {new: true, runValidators: true})
+
+        if(!user) {
+            return res.status(404).json({ message: 'utilisateur non trouvé'});
+        }
+
+        res.status(200).json({ message: 'utilisateur mis à jour avec succès', user})
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'utilisateur.', error: error.message });
+    }
+};
+
+module.exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if(!user) {
+            return res.status(404).json({message : 'utilisateurs non trouvé'})
+        }
+
+        res.status(200).json({ message: 'Utilisateur supprimé avec succès.' });
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la suppression de l\'utilisateur.', error: error.message });
+    }
+};
---
+++ backend/models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate(v) {
+            if(!validator.isEmail(v)) throw new Error('Email non valide');
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        validate(v) {
+            if (!validator.isLength(v, { min: 4, max: 20})) throw new Error ('Le mot de passe doit avoir entre 4 et 20 caractères');
+        }
+    }
+}, {
+    timestamp: true,
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+userSchema.methods.isPasswordValid = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('/users', userSchema);
