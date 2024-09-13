f5eaf86357b964c8225de5a15a1e6144053189b5
Manu <emmanuel.donahue@hotmail.fr>
2024-09-13 16:45:44 +0200

    suite

---
+++ frontend/src/components/EditPost.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { editPost } from "../services/postService";
+
+const EditPost = ({ post, update, cancel }) => {
+    const [message, setMessage] = useState(post.message);
+    const [error, setError] = useState('');
+
+    const edit = async (e) => {
+        e.preventDefault();
+
+        if (!message.trim()) {
+            setError('veuillez éditer votre message.');
+            return;
+          }
+
+        try {
+            const token = localStorage.getItem('token');
+
+            if(!token) {
+                setError('erreur, veuillez vous connecter');
+                return;
+            }
+
+            const updatePost = await editPost(post._id, { message }, token);
+            update(updatePost);
+
+        } catch (err) {
+            setError('erreur lors de la modification du message');
+        }
+    }
+
+    return <>
+        <form onSubmit={edit}>
+            <textarea 
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Modifier votre message"
+            ></textarea>
+            <button type="submit">Modifier</button>
+            <button type="button" onClick={cancel}>Annuler</button>
+        </form>
+        {error && <p>{error}</p>}
+    </>
+}
+
+export default EditPost;
--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { getPosts } from "../services/postService";
+import { getPosts, deletePost } from "../services/postService";
 import SendMessage from "./SendMessage";
+import EditPost from "./EditPost";
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
+    const [editPost, setEditPost] = useState(null);
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
                 const data = await getPosts();
+                console.log('data :', data);                
                 setPosts(data);
             } catch(err) {
                 console.log(err.message);
@@ -18,18 +21,55 @@ const Posts = () => {
         fetchPosts();
     }, []);
 
+    console.log("localStorage:", localStorage.getItem('userId'));
+
     const newMessage = (newPost) => {
         setPosts([...posts, newPost]);
-    }
+    };
+
+    const editMessage = (postId) => {
+        setEditPost(postId);
+    };
+
+    const deleteMessage = async (postId) => {
+        try {
+            const token = localStorage.getItem('token');
+            console.log("token :", token);
+            await deletePost(postId, token);
+            setPosts(posts.filter((post) => post._id !== postId));
+
+        } catch (err) {
+            console.log(err);            
+        }
+    };
+
+    const updateMessage = (update) => {
+        setPosts(posts.map((post) => (post._id === update._id ? update : post)));
+        setEditPost(null);
+    };
 
     return <>
         <h1>Hello</h1>
         <ul>
              {posts.map((post) => (
                  <li key={post._id}>
-                     <h3>{post.title}</h3>
-                     <p>{post.message}</p>
-                     <p>De {post.author?.username}</p>
+                    {editPost === post._id ? (
+                        <EditPost post={post} onUpdate={updateMessage} onCancel={() => setEditPost(null)} />
+                    ) : (<>
+                            <h3>{post.title}</h3>
+                            <p>{post.message}</p>
+                            <p>De {post.author?.username || "Auteur inconnu"}</p>
+
+                            {/* VERIFIER SI L UTILISATEUR EST BIEN LE CREATEUR DU MESSAGE */}
+                            {post.author && post.author._id && localStorage.getItem('userId') === post.author._id && (
+                                <>
+                                    <button onClick={() => editMessage(post._id)}>Modifier</button>
+                                    <button onClick={() => deleteMessage(post._id)}>Supprimer</button>
+                                </>
+                            )}
+                        </>
+                    )}
+
                  </li>
              ))}
         </ul>
